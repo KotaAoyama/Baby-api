1bbaf0a49080ac2b7223bcf3d60f40d19e3426f8
Kota Aoyama <hongcha811@gmail.com>
2020-03-23 17:33:36 +0900

    deleted unnecessary lines

--- models/migrations/20200125172013_add_babies_table.js
+++ models/migrations/20200125172013_add_babies_table.js
@@ -1,9 +1,7 @@
 exports.up = function(knex) {
   return knex.schema.createTable("babies", (t) => {
     t.increments().index();
-
     t.string("baby_name", 100).notNullable();
-
     t.string("country_code", 2).notNullable();
   });
 };
